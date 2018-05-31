44bc69c1bca94ca42ea52754d3d130b6e532f874
Amir HTH <uo240753@uniovi.es>
2018-05-31 21:50:17 +0200

    avanti!

--- wordcountjava/src/main/java/org/apache/hadoop/examples/MyWordCountMultiFile.java
+++ wordcountjava/src/main/java/org/apache/hadoop/examples/MyWordCountMultiFile.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 
 public class MyWordCountMultiFile {
@@ -18,8 +19,8 @@ public class MyWordCountMultiFile {
 
   public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+    //private final static IntWritable one = new IntWritable(1);
+    //private Text word = new Text();
 
     public void map(Object key, Text value, Context context ) throws IOException, InterruptedException {
       
@@ -31,9 +32,9 @@ public class MyWordCountMultiFile {
       
       String line = value.toString().trim();
       String[] fields = line.split(";");
-      String firstName = fields[17];
-      word.set(firstName);
-      context.write(word, one);
+      String distrito = fields[17];
+      //word.set(distrito);
+      context.write(new Text(distrito), new IntWritable(1));
 
     }
   }
@@ -58,15 +59,16 @@ public class MyWordCountMultiFile {
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     //conf.set("key.value.separator.in.input.line", ";");
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
     
-    
-    
-    Job job = new Job(conf, "word count");
+    Job job = new Job(conf, "MyWordCountMultiFile");
 
     job.setJarByClass(MyWordCountMultiFile.class);
-    job.setMapperClass(TokenizerMapper.class);
+    //job.setMapperClass(TokenizerMapper.class);
     //job.setMapperClass(Map.class);
     job.setCombinerClass(IntSumReducer.class);
+    job.setNumReduceTasks(2);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
@@ -74,10 +76,24 @@ public class MyWordCountMultiFile {
     //MultipleInputs.addInputPath(job, new Path(args[0]), FileInputFormat.class, TokenizerMapper.class);
     //MultipleInputs.addInputPath(conf, new Path(args[1]), FileInputFormat.class);
     
-    MultipleInputs.addInputPath(job, new Path(args[0]), FileInputFormat.class, TokenizerMapper.class);
-    MultipleInputs.addInputPath(job, new Path(args[1]), FileInputFormat.class, TokenizerMapper.class);
-    
-    FileOutputFormat.setOutputPath(job, new Path(args[2]));
+    if (args.length>=1 ) {
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[0]));
+    } 
+
+    if (args.length>=2 ) {
+        MultipleInputs.addInputPath(job, new Path(otherArgs[1]), FileInputFormat.class, TokenizerMapper.class);
+        job.setMapperClass(TokenizerMapper.class);
+    }
+
+    if (args.length>=3 ) {
+        MultipleInputs.addInputPath(job, new Path(otherArgs[2]), FileInputFormat.class, TokenizerMapper.class);
+        job.setMapperClass(TokenizerMapper.class);
+    }
+
+    if (args.length>=4 ) {
+        MultipleInputs.addInputPath(job, new Path(otherArgs[3]), FileInputFormat.class, TokenizerMapper.class);
+        job.setMapperClass(TokenizerMapper.class);
+    }
     
     
     System.exit(job.waitForCompletion(true) ? 0 : 1);
